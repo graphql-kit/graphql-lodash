01bb5354468f8d314ba91a829a9e35ef799f7a12
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-04-20 17:47:01 +0300

    Add more transformation in IDL

--- src/index.tsx
+++ src/index.tsx
@@ -66,24 +66,6 @@ function getLodashDirectiveArgs(fieldNode) {
   return orderedArgs;
 }
 
-const transformations = {
-  Array: {
-    map: (array, arg) => _.map(array, arg),
-    keyBy: (array, arg) => _.keyBy(array, arg),
-  },
-  Object: {
-    get: (object, arg) => _.get(object, arg),
-    mapValues: (object, arg) => _.mapValues(object, arg),
-  }
-};
-
-const transformationToType = {};
-for (const type in transformations) {
-  for (const name in transformations[type]) {
-    transformationToType[name] = type;
-  }
-}
-
 function applyLodashDirective(pathToArgs, data) {
   if (data === null)
     return null;
@@ -146,15 +128,108 @@ function stripQuery(queryAST) {
 }
 
 const lodashIDL = `
-directive @_(
-  map: String
-  keyBy: String
-  join: String = ","
+ scalar Path
+ scalar JSON
 
-  get: String
-  mapValues: String
+ enum DummyArgument {
+   _
+ }
+
+directive @_(
+  map: Path
+  keyBy: Path
+
+  # Creates an array of elements split into groups the length of size.
+  # If array can't be split evenly, the final chunk will be the remaining elements.
+  chunk: Int
+
+  # Creates a slice of array with n elements dropped from the beginning.
+  drop: Int
+
+  # Creates a slice of array with n elements dropped from the end.
+  dropRight: Int
+
+  # Creates a slice of array with n elements taken from the beginning.
+  take: Int
+
+  # Creates a slice of array with n elements taken from the end.
+  takeRight: Int
+
+  # Recursively flatten array up to depth times.
+  flatten: Int
+
+  # The inverse of \`toPairs\`; this method returns an object composed from key-value
+  # pairs.
+  fromPairs: DummyArgument
+
+  # Gets the element at index n of array. If n is negative, the nth element from
+  # the end is returned.
+  nth: Int
+
+  # Reverses array so that the first element becomes the last, the second element
+  # becomes the second to last, and so on.
+  reverse: DummyArgument
+
+  # Creates a duplicate-free version of an array, in which only the first occurrence
+  # of each element is kept. The order of result values is determined by the order
+  # they occur in the array.
+  uniq: DummyArgument
+
+  uniqBy: Path
+
+  countBy: Path
+  filter: JSON
+  reject: JSON
+  groupBy: Path
+  sortBy: [Path!]
+
+  minBy: Path
+  maxBy: Path
+  meanBy: Path
+  sumBy: Path
+
+  # Converts all elements in array into a string separated by separator.
+  join: String
+
+  get: Path
+  mapValues: Path
+
+  # Creates an array of values corresponding to paths of object.
+  at: [Path!]
+  # Creates an array of own enumerable string keyed-value pairs for object.
+  toPairs: DummyArgument
+
+  # Creates an object composed of the inverted keys and values of object.
+  # If object contains duplicate values, subsequent values overwrite property
+  # assignments of previous values.
+  invert: DummyArgument
+
+  invertBy: Path
+  # Creates an array of the own enumerable property names of object.
+  keys: DummyArgument
+  # Creates an array of the own enumerable string keyed property values of object.
+  values: DummyArgument
 ) on FIELD
 `;
+
+const transformations = {
+  Array: {
+    map: (array, path) => _.map(array, path),
+    keyBy: (array, path) => _.keyBy(array, path),
+    join: (array, sep) => _.join(array, sep),
+  },
+  Object: {
+    get: (object, path) => _.get(object, path),
+    mapValues: (object, path) => _.mapValues(object, path),
+  }
+};
+
+const transformationToType = {};
+for (const type in transformations) {
+  for (const name in transformations[type]) {
+    transformationToType[name] = type;
+  }
+}
 
 export const lodashDirectiveAST = parse(new Source(lodashIDL, 'lodashIDL'));
 const lodashDirectiveDef = getDirectivesFromAST(lodashDirectiveAST)[0];
@@ -167,7 +242,33 @@ function getDirectivesFromAST(ast) {
   `;
   const fullAST = concatAST([ast, parse(dummyIDL)]);
   const schema = buildASTSchema(fullAST);
+
+  schema.getTypeMap()['Path'].parseLiteral = (x => x.value);
+  schema.getTypeMap()['JSON'].parseLiteral = astToJSON;
+
   return schema.getDirectives();
+}
+
+// TODO: copy-pasted from JSON Faker move to graphql-js or separate lib
+function astToJSON(ast) {
+  switch (ast.kind) {
+    case Kind.NULL:
+      return null;
+    case Kind.INT:
+      return parseInt(ast.value, 10);
+    case Kind.FLOAT:
+      return parseFloat(ast.value);
+    case Kind.STRING:
+    case Kind.BOOLEAN:
+      return ast.value;
+    case Kind.LIST:
+      return ast.values.map(astToJSON);
+    case Kind.OBJECT:
+      return ast.fields.reduce((object, {name, value}) => {
+        object[name.value] = astToJSON(value);
+        return object;
+      }, {});
+  }
 }
 
 function traverseOperation(queryAST, operationName, visitor) {
