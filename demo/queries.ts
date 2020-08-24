812988c7da9e258fbe2d688185c61c6f16735a6c
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-08-24 15:42:44 +0300

    Remove dedent as dependency

--- demo/queries.ts
+++ demo/queries.ts
@@ -1,56 +1,56 @@
-import dedent from 'dedent';
+export const defaultQuery = `
+# This is sample query with @_ directives which change the shape of response
+# Run this query to see the resutls!
+# More examples are hidden under Saved Queries dropdown above
+{
+  genderStats: allPeople @_(get: "people") {
+    people @_(countBy: "gender") {
+      gender
+    }
+  }
+}`.trim();
 
-export const defaultQuery = dedent`# This is sample query with @_ directives which change the shape of response
-                                   # Run this query to see the resutls!
-                                   # More examples are hidden under Saved Queries dropdown above
-                                   {
-                                     genderStats: allPeople @_(get: "people") {
-                                       people @_(countBy: "gender") {
-                                         gender
-                                       }
-                                     }
-                                   }`;
+const maxPopulationQuery = `
+# Planet With Max Population
+{
+  planetWithMaxPopulation: allPlanets @_(get: "planets") {
+    planets @_(maxBy: "population") {
+      name
+      population
+    }
+  }
+}`.trim();
+
+const genderStatsQuery = `
+# Gender Stats
+{
+  genderStats: allPeople @_(get: "people") {
+    people @_(countBy: "gender") {
+      gender
+    }
+  }
+}`.trim();
+
+const mapPeopleToFilmsQuery = `
+# Map People To Films
+{
+  peopleToFilms: allPeople @_(get: "people") {
+    people @_(
+      keyBy: "name"
+      mapValues: "filmConnection.films"
+    ) {
+      name
+      filmConnection {
+        films @_(map: "title") {
+          title
+        }
+      }
+    }
+  }
+}`.trim();
 
 export const savedQueries = [
-{
-  query: dedent`# Planet With Max Population
-                {
-                  planetWithMaxPopulation: allPlanets @_(get: "planets") {
-                    planets @_(maxBy: "population") {
-                      name
-                      population
-                    }
-                  }
-                }`,
-  variables: ''
-},
-  {
-    query: dedent`# Gender Stats
-                  {
-                    genderStats: allPeople @_(get: "people") {
-                      people @_(countBy: "gender") {
-                        gender
-                      }
-                    }
-                  }`,
-  variables: ''
-},
-{
-  query: dedent`# Map People To Films
-                {
-                  peopleToFilms: allPeople @_(get: "people") {
-                    people @_(
-                      keyBy: "name"
-                      mapValues: "filmConnection.films"
-                    ) {
-                      name
-                      filmConnection {
-                        films @_(map: "title") {
-                          title
-                        }
-                      }
-                    }
-                  }
-                }`,
-  variables: ''
-}];
+  { query: maxPopulationQuery, variables: "" },
+  { query: genderStatsQuery, variables: "" },
+  { query: mapPeopleToFilmsQuery, variables: "" }
+];
